7ac96255f18cd61f63d164e5dcbc85352cbabdde
Dinupdk <pdk7893@gmail.com>
2025-04-23 12:23:29 +0530

    Signed-off-by: Dinupdk <pdk7893@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Footer = () => {
   const year = new Date().getFullYear();
   
@@ -10,10 +11,9 @@ const Footer = () => {
       <div className="container max-w-7xl mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <h3 className="font-semibold text-lg mb-4">BloggerClone</h3>
+            <h3 className="font-semibold text-lg mb-4">Dinesh Blogs</h3>
             <p className="text-gray-600">
-              A simple blogging platform inspired by Google Blogger.
-              Share your thoughts with the world.
+              A simple blogging by Dinesh .
             </p>
           </div>
           
@@ -53,14 +53,41 @@ const Footer = () => {
               </li>
             </ul>
           </div>
+         
+          <div className="container mx-auto text-center">
+        <div className="social-icons flex justify-center items-center space-x-4">
+          <a href="https://www.linkedin.com/in/dineshpdk789/" target="_blank" rel="noopener noreferrer">
+            <img src='public\linkedin.png' className="w-6 h-6 text-blue-500 hover:text-blue-700" />
+          </a>
+          <a href="https://www.instagram.com/dineshpdk789/" target="_blank" rel="noopener noreferrer">
+            <img src='public\instagram.png' className="w-6 h-6 text-pink-500 hover:text-pink-700" />
+          </a>
+          <a href="https://github.com/Dinupdk/" target="_blank" rel="noopener noreferrer">
+            <img src='public\github.png' className="w-6 h-6 text-pink-500 hover:text-pink-700" />
+          </a>
+          <a href="https://x.com/Dineshpdk789/" target="_blank" rel="noopener noreferrer">
+            <img src='public\twitter.png' className="w-6 h-6 text-pink-500 hover:text-pink-700" />
+          </a>
+          <a href="mailto:dineshpdk7893@gmail.com" target="_blank" rel="noopener noreferrer">
+            <img src='public\gmail.png' className="w-6 h-6 text-pink-500 hover:text-pink-700" />
+          </a>
+
+
+        </div>
+       
+      </div>
+            
+         
         </div>
         
         <div className="border-t border-blog-border mt-8 pt-8 text-center text-gray-600">
-          <p>&copy; {year} BloggerClone. All rights reserved.</p>
+          <p>&copy; {year} Dinesh Blogs. All rights reserved.</p>
         </div>
       </div>
+      
     </footer>
   );
 };
 
 export default Footer;
+
